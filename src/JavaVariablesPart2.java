84d02f217d786eb7e03707cfdb7837315b49dc06
James910066 <james.dinser@gmail.com>
2023-02-01 19:03:23 -0500

    Initial commit

---
+++ src/JavaVariablesPart2.java
@@ -0,0 +1,20 @@
+public class JavaVariablesPart2
+{
+    public static void main(String[] args)
+    {
+        int kidQuantity = 3;
+        boolean isRaining = false;
+        double gasPrice = 3.87;
+        int myFavNumber = 21;
+        double SHOE_SIZE = 9.5;
+        String MY_BIRTH_MONTH = "August";
+        String myFullName = "James Herbert Dinser";
+        System.out.println("The number of kids I have is " + kidQuantity);
+        System.out.println("T/F Is it raining today? " + isRaining);
+        System.out.println("A gallon of gas right now cost $" + gasPrice);
+        System.out.println("My favorite number is " + myFavNumber);
+        System.out.println("The size of my shoe is " + SHOE_SIZE);
+        System.out.println("I was born in the month of " + MY_BIRTH_MONTH);
+        System.out.println("My full name is " + myFullName);
+    }
+}
